316f4b2e2888940f30b8e041ff66295ac174596c
Doyle D <doyle.jr2@gmail.com>
2014-03-06 09:49:25 -0500

    Area Calc
    Just a basic area calculator, just brushing up on my skills

---
+++ AreaCalculator/AreaCalculator/OutputRow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator
+{
+    class OutputRow
+    {
+        public string CustomerName ;
+        public decimal Width;
+        public decimal Length;
+        public decimal Area;
+        public decimal Price;
+
+        public void SetData(string inputData, decimal pricePerSquareFoot)
+        {
+            string[] data = inputData.Split(',');
+
+            CustomerName = data[0];
+            Width = decimal.Parse(data[1]);
+            Length = decimal.Parse(data[2]);
+
+            Area = Length*Width;
+            Price = Area*pricePerSquareFoot;
+        }
+
+        public string CreateOutputRow()
+        {
+            return string.Format("{0},{1},{2},{3},{4:C}", CustomerName, Width, Length, Area, Price);
+        }
+    }
+}
---
+++ AreaCalculator/AreaCalculator/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator
+{
+    class Program
+    {
+        const string path = @"..\..\";
+
+        static void Main(string[] args)
+        {
+              // AreaCalculator\bin\debug   We have to go up two directories 
+            decimal costPerSquareFoot;
+            string inputFileName;
+            string outputFileName;
+
+            inputFileName = GetInputFile();
+            outputFileName = GetOutputFileName();
+            costPerSquareFoot = GetCostPerSquareFoot();
+
+            string[] lines = File.ReadAllLines(inputFileName);
+
+            StreamWriter writer = File.AppendText(path + outputFileName);
+            writer.WriteLine("CustomerName,Width,Length,Area,Price");
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                OutputRow row = new OutputRow();
+                row.SetData(lines[i], costPerSquareFoot);
+                writer.WriteLine(row.CreateOutputRow());
+            }
+
+            writer.Close();
+        }
+
+        private static decimal GetCostPerSquareFoot()
+        {
+            bool validDecimal = false;
+            decimal output = 0;
+
+            while (!validDecimal)
+            {
+                Console.WriteLine("Enter cost per square foot: ");
+                if (decimal.TryParse(Console.ReadLine(), out output))
+                    validDecimal = true;
+                else
+                    Console.WriteLine("Bad data!  Decimal stupid!");
+            }
+
+            return output;
+        }
+
+        static string GetOutputFileName()
+        {
+            Console.WriteLine("Name of output file: ");
+            return Console.ReadLine();
+        }
+
+        static string GetInputFile()
+        {
+            bool fileExists = false;
+            string inputFileName ="";
+
+            while (!fileExists)
+            {
+                Console.Write("Provide input file name: ");
+                inputFileName = Console.ReadLine();
+                fileExists = File.Exists(path + inputFileName);
+            }
+
+            return path + inputFileName;
+        }
+    }
+}
